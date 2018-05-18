1275fde84b87307ed3a399068f233d6e9f5ce6eb
unknown <amkadesky@aol.com>
2018-05-18 15:20:51 -0500

    created first auth test

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const passport = require('passport');
 mongoose.Promise = global.Promise;
 
 
@@ -20,6 +21,23 @@ const jsonParser = bodyParser.json;
 app.use(express.static('public'));
 
 app.use('/users', usersRouter);
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+})
+
+// passport.use(localStrategy);
+// passport.use(jwtStrategy);
+
+// app.use('/auth', authRouter);
+
+// const jwtAuth = passport.authenticate('jwt', {session: false})
 
 let server;
 
