2b28a47c16369f43ecb7b23e94056f1e95361db8
unknown <amkadesky@aol.com>
2018-05-23 20:45:16 +0100

    changed endpoint to gh-pages from localhost

--- home.js
+++ home.js
@@ -1,10 +1,10 @@
 'use strict';
 function userCreate() {
 	console.log('running user create function');
-	const endpoint = 'http://localhost:8080/users';
+	const endpoint = 'https://zabaow.github.io/Inventation/users';
 	var xhttp = new XMLHttpRequest();
 	xhttp.open('GET', endpoint, true);
 	xhttp.send();
-	// var response = JSON.parse(xhttp.resonseText);
-	// console.log(response);
+	var response = JSON.parse(xhttp.responseText);
+	console.log(response);
 }
