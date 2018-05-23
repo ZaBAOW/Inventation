738593a62e993c8f3d8123baa23eb8f786c03b30
unknown <amkadesky@aol.com>
2018-05-23 18:30:14 +0100

    created userCreate method

--- home.js
+++ home.js
@@ -1,11 +1,10 @@
 'use strict';
-const server = require('./server');
 function userCreate() {
 	console.log('running user create function');
-	const endpoint = 'localhost:8080/users';
+	const endpoint = 'http://localhost:8080/users';
 	var xhttp = new XMLHttpRequest();
 	xhttp.open('GET', endpoint, true);
 	xhttp.send();
-	var response = JSON.parse(xhttp.resonseText);
-	console.log(response);
+	// var response = JSON.parse(xhttp.resonseText);
+	// console.log(response);
 }
