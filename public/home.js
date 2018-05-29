e5e55b8906c0f03a4aae3f5daf922b5249778426
unknown <amkadesky@aol.com>
2018-05-29 12:58:01 -0500

    createUser() completes
    however, there is a 422 error that appears
    due to how the response is parsed by the
    application

--- public/home.js
+++ public/home.js
@@ -1,11 +1,37 @@
 'use strict';
-const HOST_NAME = process.env.HOST_NAME;
-function userCreate() {
-	console.log('running user create function');
+const HOST_NAME = '/users';
+function getUsers() {
+	console.log('running get user function');
 	const endpoint = HOST_NAME;
 	var xhttp = new XMLHttpRequest();
 	xhttp.open('GET', endpoint, true);
 	xhttp.send();
 	var response = JSON.parse(xhttp.responseText);
 	console.log(response);
+}
+
+function userCreate() {
+	var username = $('.username').val();
+	var password = $('.password').val();
+	var firstName = $('.firstName').val();
+	var lastName = $('.lastName').val();
+	const endpoint = '/users';
+	console.log('running user create function');
+	fetch(endpoint , {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({username: username, password: password, firstName: firstName, lastName: lastName})
+	})
+	.then(function(response) {
+		var content = response.json();
+		console.log(content);
+		return content;
+	});
+
+	// const content = await rawResponse.json();
+
+	// console.log(content)
 }
