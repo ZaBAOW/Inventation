1f89eb537ef7b8f271a90e7ae4afb89e83e526d5
unknown <amkadesky@aol.com>
2018-09-25 19:54:39 -0500

    specified test database for auth tests

--- test/test-auth.js
+++ test/test-auth.js
@@ -15,98 +15,98 @@ chai.use(chaiHttp);
 
 
 describe('Auth endpoints', function () {
-	const username = 'exampleUser';
-	const password = 'examplePassword';
-	const firstName = 'Example';
-	const lastName = 'User';
+    const username = 'exampleUser';
+    const password = 'examplePassword';
+    const firstName = 'Example';
+    const lastName = 'User';
 
-	before(function () {
-		return runServer();
-	});
+    before(function () {
+        return runServer(TEST_DATABASE_URL);
+    });
 
-	after(function() {
-		return closeServer();
-	});
+    after(function() {
+        return closeServer();
+    });
 
-	beforeEach(function() {
-		return User.hashPassword(password).then(password => User.create({
-			username,
-			password,
-			firstName,
-			lastName
-		})
-		);
-	});
+    beforeEach(function() {
+        return User.hashPassword(password).then(password => User.create({
+            username,
+            password,
+            firstName,
+            lastName
+        })
+        );
+    });
 
-	afterEach(function () {
-		return User.remove({});
-		localStorage.clear();
-		localStorage.itemInsertionCallback = null;
-	});
+    afterEach(function () {
+//        return User.remove({});
+        localStorage.clear();
+        localStorage.itemInsertionCallback = null;
+    });
 
-	describe('/auth/login', function () {
-		it('Should return a valid auth token', function(){
-			return chai.request(app).post('/auth/login')
-			.send({username, password})
-			.then(res => {
-				expect(res).to.have.status(200);
-				expect(res.body).to.be.an('object');
-				const token = res.body.authToken;
-				expect(token).to.be.a('string');
-				const payload = jwt.verify(token, JWT_SECRET, {
-					algorithm: ['HS256']
-				});
-			});
-		});
+    describe('/auth/login', function () {
+        it('Should return a valid auth token', function(){
+            return chai.request(app).post('/auth/login')
+            .send({username, password})
+            .then(res => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('object');
+                const token = res.body.authToken;
+                expect(token).to.be.a('string');
+                const payload = jwt.verify(token, JWT_SECRET, {
+                    algorithm: ['HS256']
+                });
+            });
+        });
 
-		it('Should return a valid auth token with a newer expiry date', function() {
-			const token = jwt.sign(
-				{
-					user: {
-						username,
-						firstName,
-						lastName
-				}
-				},
-					JWT_SECRET,
-				{
-					algorithm: 'HS256',
-					subject: username,
-					expiresIn: '7d'
-				}
-			);
-			const decoded = jwt.decode(token);
+        it('Should return a valid auth token with a newer expiry date', function() {
+            const token = jwt.sign(
+                {
+                    user: {
+                        username,
+                        firstName,
+                        lastName
+                }
+                },
+                    JWT_SECRET,
+                {
+                    algorithm: 'HS256',
+                    subject: username,
+                    expiresIn: '7d'
+                }
+            );
+            const decoded = jwt.decode(token);
 
-			return chai.request(app).post('/auth/refresh')
-			.set('authorization', `Bearer ${token}`)
-			.then(res => {
-				expect(res).to.have.status(200);
-				expect(res.body).to.be.an('object');
-				const token = res.body.authToken;
-				expect(token).to.be.a('string');
-				const payload = jwt.verify(token, JWT_SECRET, {
-					algorithm: ['HS256']
-				});
-				expect(payload.user).to.deep.equal({
-					username,
-					firstName,
-					lastName
-				});
-				expect(payload.exp).to.be.at.least(decoded.exp);
-			});
-		});
+            return chai.request(app).post('/auth/refresh')
+            .set('authorization', `Bearer ${token}`)
+            .then(res => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('object');
+                const token = res.body.authToken;
+                expect(token).to.be.a('string');
+                const payload = jwt.verify(token, JWT_SECRET, {
+                    algorithm: ['HS256']
+                });
+                expect(payload.user).to.deep.equal({
+                    username,
+                    firstName,
+                    lastName
+                });
+                expect(payload.exp).to.be.at.least(decoded.exp);
+            });
+        });
 
-		it('Should store valid auth token in cookie', function() {
-			return chai.request(app).post('/auth/login')
-			.send({username, password})
-			.then(res => {
-				const sessionObj = res.headers['set-cookie'][0];
-				const re = /=(.*?)=/;
-				const cookieToken = sessionObj.match(re);
-				const compareCookie = cookieToken[1];
-				expect(res).to.have.status(200);
-				expect(res).to.be.an('object');
-			})
-		});
-	});
+        it('Should store valid auth token in cookie', function() {
+            return chai.request(app).post('/auth/login')
+            .send({username, password})
+            .then(res => {
+                const sessionObj = res.headers['set-cookie'][0];
+                const re = /=(.*?)=/;
+                const cookieToken = sessionObj.match(re);
+                const compareCookie = cookieToken[1];
+                expect(res).to.have.status(200);
+                expect(res).to.be.an('object');
+            })
+        });
+    });
 });
